360fba91e9e6d5209e07cb2c612cc79b7a0a7f09
Bunlai Kek <kekbunlai@gmail.com>
2020-12-04 17:06:43 +0700

    add table layout

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  items = [
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"},
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"},
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"},
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"},
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"},
+    {no: 1, service: "Handed Massage", desc: "Khmer traditional massage by hand of the handicape", merchant: "VIP Massage", status: "Available", createdAt: "04/12/2020", updatedAt: "04/12/2020"}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
